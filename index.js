038e6ffa772706f50005f655be4cb4fc7b43d579
ShehabGamal689 <72823848+ShehabGamal689@users.noreply.github.com>
2026-05-18 06:21:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Nawy From Shehab Gamal Baioumy\n'
+  const msg = 'DevOps Engineer\n'
   res.end(msg);
 });
 
